68371774dd2d5175bf2b757eb0f67c89bde49185
Andrew Jaworski <apjawors@gmail.com>
2023-03-01 23:33:58 -0500

    Prototype of app created

---
+++ main.js
@@ -0,0 +1,29 @@
+let slider = document.getElementById('slider')
+let rangeDisplay = document.getElementById('range-display')
+let grid = document.getElementById('grid')
+let erase = document.querySelector('button')
+
+slider.addEventListener('input', () => {
+    rangeDisplay.innerHTML = `${slider.value}x${slider.value}` 
+    let area = slider.value * slider.value
+    let items = []
+
+    for (let i = 0; i < area; i++){
+        let div = document.createElement('div')
+        items.push(div)
+    }
+
+    grid.replaceChildren(...items)
+    grid.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`
+
+})
+
+erase.addEventListener('click', (e) => {
+    let squares = grid.childNodes
+    for (let i = 0; i < squares.length; i++) {
+        if (squares[i].nodeName.toLowerCase() == 'div') {
+            squares[i].style.background = 'none';
+        }
+    }
+})
